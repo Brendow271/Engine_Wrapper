a49b303c5d3a52a65e116b91b93c461dd92fd921
Brendow271 <pashashkuropat@yandex.ru>
2023-11-13 21:31:21 +0300

    add unit test code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Subject subj;
+
+    Wrapper wrapper(&subj, &Subject::f3, {{"arg1", 0}, {"arg2", 0}});
+    Engine engine;
+
+    engine.register_command(&wrapper, "command1");
+
+    std::cout << engine.execute("command1", {{"arg1", 4}, {"arg2", 5}}) << std::endl;
+
     return 0;
-}
+};
