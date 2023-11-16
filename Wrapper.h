a92f7e5694c7c09668140f381faac16e7809d97b
Brendow271 <pashashkuropat@yandex.ru>
2023-11-16 21:41:20 +0300

    add some comments

--- Wrapper.h
+++ Wrapper.h
@@ -8,6 +8,7 @@
 template<typename T>
 class Wrapper{
 public:
+    //template has been applied to c-tor
     template<typename Obj, typename... Args>
     Wrapper(Obj *object, T(Obj::*func)(Args...), std::vector<std::pair<std::string, T>> const & inArgs) {
         for (auto& arg : inArgs) {
@@ -17,12 +18,15 @@ public:
         {
             throw std::runtime_error("ERROR[wrapper]: The names of the arguments should not be repeated");
         }
+        //lambda func create here to store these templates
         command = [this, object, func](std::vector<T>& argsForFunc) {
+            //std:: make_index_sequence returns a sequence from 0 to sizeof(args) - 1
             return executeFunc(object, func, argsForFunc, std::make_index_sequence<sizeof...(Args)>{});
         };
     }
 
     template<typename Obj, typename FuncName, size_t... I>
+    //variadic template collect indexes to get values from the vector inArgs[I]
     T executeFunc(Obj *object, FuncName funcName, std::vector<T>& inArgs, std::index_sequence<I...>) {
         return ((object->*funcName)(inArgs[I]...));
     }
--- main.cpp
+++ main.cpp
@@ -11,11 +11,13 @@ int main() {
     Engine engine;
     engine.register_command<int>(&wrapper, "command1");
     std::cout << engine.execute<int>("command1", {{"arg1", 4}, {"arg2", 5}}) << std::endl;
-
-    Wrapper<double> wrapperD(&subj, &Subject::f3d, {{"arg1", 0}, {"arg2", 0}});
+    //result: 1024
+    //work with double elements
+    Wrapper<double> wrapperD(&subj, &Subject::f3d, {{"arg1", 0}, {"arg2", 0},{"arg3",0}});
     Engine engineD;
     engineD.register_command<double>(&wrapperD, "command1");
     std::cout << engineD.execute<double>("command1", {{"arg1", 4}, {"arg2", 5}}) << std::endl;
+    //result: 0.8
 
     return 0;
 };
